6744bc5c1dc3bdd5be265cd9af250567ac792dcb
Tulio Guimaraes <tulio.guimaraes@dnit.gov.br>
2024-12-23 14:54:43 -0300

    [refs:#6956] Plugin SEI - Rotas - Ajuste da página de configuração para que o usuário possa configurar as novas rotas

--- src/js/options.js
+++ src/js/options.js
@@ -1,4 +1,7 @@
 import * as alert from 'view/alert.js';
+import { loadTrelloRoutes, listRoutes, clearRoutes } from "model/routes_store.js";
+import routesId from "model/routes_id.js";
+import { getRoute } from "model/routes_store.js";
 
 import 'css/options.scss';
 
@@ -15,9 +18,15 @@ const mapUI = () => {
   ui.lblNoAppKeyInfo = document.getElementById('lbl-no-app-key-info');
   ui.defaultBoard = document.getElementById('txt-default-board');
   ui.defaultList = document.getElementById('txt-default-list');
-  ui.btnSave = document.getElementById('btn-save');
+  //ui.btnSave = document.getElementsByClassName('btn-salvar-config');
+  ui.btnTrelloRoutes = document.getElementById('btn-trello-routes');
+  ui.btnClearRoutes = document.getElementById('btn-clear-routes');
 
-  ui.btnSave.addEventListener('click', save);
+  for (const btnSave of Array.prototype.slice.call(document.getElementsByClassName('btn-salvar-config'))) {
+    btnSave.addEventListener('click', save);
+  }
+  ui.btnTrelloRoutes.addEventListener('click', loadDefaultRoutes);
+  ui.btnClearRoutes.addEventListener('click', clearConfiguredRoutes);
 
   ui.appKey.addEventListener('input', () => {
     updateTokenUrl();
@@ -26,6 +35,114 @@ const mapUI = () => {
   document.addEventListener('DOMContentLoaded', () => {
     updateTokenUrl();
   });
+};
+
+const loadRoutesForm = () => {
+  document.forms['form-routes'].innerHTML = '';
+  const routes = Object.values(routesId);
+  routes.forEach(async (route) => {
+    const routeId = route.id;
+    const routeShortDesc = route.shortdesc;
+    const savedRoute = await getRoute(routeId);
+    const routeFieldset = document.createElement('fieldset');
+    routeFieldset.className = 'border p-2';
+    const routeLegend = document.createElement('legend');
+    routeLegend.textContent = routeShortDesc;
+    routeLegend.className = 'w-auto';
+    const routeOuterDiv = document.createElement('div');
+    routeOuterDiv.className = 'form-row';
+    routeOuterDiv.appendChild(getRouteUrlField(routeId, savedRoute.url));
+    routeOuterDiv.appendChild(getRouteVerbField(routeId, savedRoute.verb));
+    routeOuterDiv.appendChild(getRouteBodyField(routeId, savedRoute.body));
+    routeFieldset.appendChild(routeLegend);
+    routeFieldset.appendChild(routeOuterDiv);
+    document.forms['form-routes'].appendChild(routeFieldset);
+  });
+};
+
+const getRouteBodyField = (index, savedBody) => {
+  const ident = `rota-${index}-body`;
+  const outerdiv = document.createElement('div');
+  outerdiv.className = 'form-row';
+  const innerDiv = document.createElement('div');
+  innerDiv.className = 'col';
+  // Monta o label do campo
+  const label = document.createElement('label');
+  label.textContent = 'Request Body';
+  label.htmlFor = ident;
+  // Monta o campo de texto
+  const textarea = document.createElement('textarea');
+  textarea.className = 'form-control';
+  textarea.id = ident;
+  textarea.name = ident;
+  textarea.rows = 5;
+  textarea.placeholder = 'JSON Body';
+  if (savedBody !== undefined) {
+    textarea.value = savedBody;
+  }
+  //Adiciona os elementos ao div
+  innerDiv.appendChild(label);
+  innerDiv.appendChild(textarea);
+  return innerDiv;
+};
+
+const getRouteVerbField = (index, savedVerb) => {
+  const ident = `rota-${index}-verb`;
+  const div = document.createElement('div');
+  div.className = 'col-md-3';
+  // Monta o label do campo
+  const label = document.createElement('label');
+  label.textContent = 'Verbo';
+  label.htmlFor = ident;
+  // Monta o campo do select
+  const select = getVerbSelectElement(index, savedVerb);
+  //Adiciona os elementos ao div
+  div.appendChild(label);
+  div.appendChild(select);
+  return div;
+};
+
+const getRouteUrlField = (index, savedUrl) => {
+  const ident = `rota-${index}-url`;
+  const div = document.createElement('div');
+  div.className = 'col-md-9';
+  // Monta o label do campo
+  const label = document.createElement('label');
+  label.textContent = 'Rota';
+  label.htmlFor = ident;
+  // Monta o campo de texto
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'form-control';
+  input.id = ident;
+  input.name = ident;
+  input.placeholder = 'URL da rota';
+  if (savedUrl !== undefined) {
+    input.value = savedUrl;
+  }
+  //Adiciona os elementos ao div
+  div.appendChild(label);
+  div.appendChild(input);
+  return div;
+};
+
+const getVerbSelectElement = (index, savedBody) => {
+  const select = document.createElement('select');
+  const id = `rota-${index}-verb`;
+  select.id = id;
+  select.name = id;
+  select.className = 'form-control';
+  const options = ['GET', 'POST', 'PUT', 'DELETE'];
+  options.forEach((option) => {
+    const opt = document.createElement('option');
+    opt.value = option;
+    opt.textContent = option;
+    if (option === savedBody) {
+      opt.selected = true;
+    }
+    select.appendChild(opt);
+  });
+  return select;
 };
 
 const updateTokenUrl = () => {
@@ -43,16 +160,47 @@ const updateTokenUrl = () => {
   }
 };
 
-const save = (e) => {
-  chrome.storage.sync.set({
+const save = async (e) => {
+  e.preventDefault();
+  const dataToSave = Object.assign({}, {
     appKey: ui.appKey.value,
     appToken: ui.appToken.value,
     defaultBoard: ui.defaultBoard.value,
     defaultList: ui.defaultList.value,
   });
+  //await clearRoutes();
+  const newRoutes = [];
+  const values = Object.values(routesId);
+  for (const value of values) {
+    const routeId = value.id;
+    const routeUrl = document.getElementById(`rota-${routeId}-url`).value;
+    const routeBody = document.getElementById(`rota-${routeId}-body`).value;
+    const routeVerb = document.getElementById(`rota-${routeId}-verb`).value;
+    newRoutes.push({ id: routeId, url: routeUrl, body: routeBody, verb: routeVerb });
+  }
+  Object.assign(dataToSave, { routes: newRoutes });
+  chrome.storage.sync.set(dataToSave);
   alert.success('As configurações foram salvas com sucesso.');
-  e.preventDefault();
+  await listRoutes();
 };
+
+const clearConfiguredRoutes = async (event) => {
+  event.preventDefault();
+  clearRoutes();
+  alert.success('Rotas configuradas foram removidas com sucesso.');
+};
+
+const loadDefaultRoutes = async (event) => {
+  event.preventDefault();
+  if (confirm('Deseja realmente carregar as configurações do Trello? Esta ação não pode ser desfeita.')) {
+    await loadTrelloRoutes();
+    loadRoutesForm();
+    await listRoutes();
+    alert.success('Rotas do Trello carregadas com sucesso.');
+  } else {
+    console.log('Rotas do Trello canceladas.');
+  }
+}
 
 const restore = () => {
   chrome.storage.sync.get(
@@ -75,4 +223,5 @@ const restore = () => {
 document.addEventListener('DOMContentLoaded', () => {
   mapUI();
   restore();
+  loadRoutesForm();
 });
