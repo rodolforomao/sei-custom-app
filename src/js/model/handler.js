3f6ab74947e1a0bd0e4865d0fdb1e6a6f50f3e15
Tulio Guimaraes <tulio.guimaraes@dnit.gov.br>
2025-02-10 15:30:10 -0300

    [refs:#7036] Plugin SEI - Ajuste no handler para permitir que a verificação se o processo está associado a um cartão seja feita por um novo campo em vez de usar o card.desc

--- src/js/model/handler.js
+++ src/js/model/handler.js
@@ -11,11 +11,20 @@ export const getCards = (cardList, filteredProcessNumber = null) => {
     let match = null;
     let description = cardFromTrello.desc;
     let processList = [];
-    while ((match = new RegExp(extractSEIInfoRegex).exec(description))) {
-      processList.push(match[1]);
-      description = removeSubstrFromStr(match.index, match.index + match[0].length, description);
+    // Verifica se o backend já retornou uma lista de processos associados a este cartão
+    if (cardFromTrello.seiNumbers) {
+      // Se já tem uma lista dos processos, vamos usa-la para identificar os números dos processos
+      processList = cardFromTrello.seiNumbers;
+    } else {
+      // Caso o backend não tenha retornado uma lista de processos, vamos extrair os números dos processos da descrição (padrão original do plugin do Trello)
+      while ((match = new RegExp(extractSEIInfoRegex).exec(description))) {
+        // Pega o número de processo e adiciona na lista de processos
+        processList.push(match[1]);
+        description = removeSubstrFromStr(match.index, match.index + match[0].length, description);
+      }
     }
     if (filteredProcessNumber) {
+      // Atenção: Esta verificação é feita somente com o número do processo, não envolve o pprefixo "SEI"
       processList = processList.filter((processNumber) => filteredProcessNumber === processNumber);
     }
     processList.forEach((processNumber) => {
