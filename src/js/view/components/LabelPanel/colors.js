895b46cc750bdc1ad3f78f871e590e3caab4cf12
Mitaly <mitaly.oliveira@hotmail.com>
2025-06-23 16:56:33 -0300

    refs[7298] - Cores etiquetas

--- src/js/view/components/LabelPanel/colors.js
+++ src/js/view/components/LabelPanel/colors.js
@@ -1,3 +1,4 @@
+
 export default {
   default: ['#b6bbbf', '#97a0af'],
   green: ['#61bd4f', '#519839'],
@@ -9,5 +10,45 @@ export default {
   sky: ['#00c2e0', '#0098b7'],
   lime: ['#51e898', '#4bbf6b'],
   pink: ['#ff80ce', '#c9558f'],
-  black: ['#4d4d4d', '#091e42'],
+  black: ['#4D4D4D', '#4D4D4D'],
+  
+  grayDark: ['#666666', '#666666'],
+  grayLight: ['#CCCCCC', '#CCCCCC'],
+  grayMedium: ['#B3B3B3', '#B3B3B3'],
+
+  redBright: ['#ed1800', '#ed1800'],
+  redSoft: ['#D33115', '#D33115'],
+  redDark: ['#9F0500', '#9F0500'],
+
+  orangeTransparent: ['#fe92008c', '#fe92008c'],
+  orangeLight: ['#E27300', '#E27300'],
+  orangeDark: ['#C45100', '#C45100'],
+
+  yellowBright: ['#FCDC00', '#FCDC00'],
+  yellowLight: ['#FCC400', '#FCC400'],
+  yellowDark: ['#d3b005', '#d3b005'],
+
+  greenBright: ['#DBDF00', '#DBDF00'],
+  greenLight: ['#B0BC00', '#B0BC00'],
+  greenDark: ['#808900', '#808900'],
+
+  greenAlt: ['#A4DD00', '#A4DD00'],
+  greenAltLight: ['#68BC00', '#68BC00'],
+  greenAltDark: ['#194D33', '#194D33'],
+
+  blueSoft: ['#68CCCA', '#68CCCA'],
+  blueLight: ['#16A5A5', '#16A5A5'],
+  blueDark: ['#0C797D', '#0C797D'],
+
+  blueAlt: ['#73D8FF', '#73D8FF'],
+  blueAltLight: ['#009CE0', '#009CE0'],
+  blueAltDark: ['#0062B1', '#0062B1'],
+
+  purpleAlt: ['#a299d9', '#a299d9'],
+  purpleLight: ['#7B64FF', '#7B64FF'],
+  purpleDark: ['#653294', '#653294'],
+
+  pinkAlt: ['#fc7dff', '#fc7dff'],
+  pinkLight: ['#FA28FF', '#FA28FF'],
+  pinkDark: ['#AB149E', '#AB149E']
 };
