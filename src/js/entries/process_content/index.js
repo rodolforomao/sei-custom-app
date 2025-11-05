1b7226bbbc24324927810ada6e38e13cb19d5ba1
Mitaly <mitaly.oliveira@hotmail.com>
2025-11-05 16:16:42 -0300

    refs[7539] - Bloqueio várias requisições - conflito seipro

--- src/js/entries/process_content/index.js
+++ src/js/entries/process_content/index.js
@@ -3,16 +3,34 @@ import 'regenerator-runtime/runtime';
 
 import * as dom from './dom.js';
 import * as controller from 'controller/trello.js';
-
 import 'css/process_content.scss';
 
-dom.prepare();
+(() => {
+  if (window._PLUGINSEI_EXT_INITIALIZED) return;
+  window._PLUGINSEI_EXT_INITIALIZED = true;
 
-const firstTrelloBox = document.querySelector('[data-trello-process-box]');
+  const CACHE_KEY = 'PLUGIN_SEI_REQUEST_TIME';
+  const MIN_INTERVAL_MS = 5000; 
 
-if (firstTrelloBox) {
-  const processNumber = firstTrelloBox.getAttribute('data-trello-process-number');
-  if (processNumber) {
-    controller.load({ processNumber: processNumber });
-  }
-}
+  const getLastRequestTime = () => parseInt(localStorage.getItem(CACHE_KEY) || '0', 10);
+  const setLastRequestTime = (time) => localStorage.setItem(CACHE_KEY, time.toString());
+
+  const initTrello = () => {
+    dom.prepare();
+
+    const box = document.querySelector('[data-trello-process-box]');
+    if (!box) return;
+
+    const processNumber = box.getAttribute('data-trello-process-number');
+    if (!processNumber) return;
+
+    const now = Date.now();
+    const last = getLastRequestTime();
+    if (now - last < MIN_INTERVAL_MS) return;
+
+    setLastRequestTime(now);
+    controller.load({ processNumber });
+  };
+
+  initTrello();
+})();
